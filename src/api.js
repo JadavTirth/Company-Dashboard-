12dbd9a19e1920a1a4012048570a2c1868c0b1e6
Tirth A. Jadav <tirth.jadav04@gmail.com>
2026-02-09 12:19:22 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+ // baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://company-dashboard-backend-xa8w.onrender.com"
 });
 
 export default API;
