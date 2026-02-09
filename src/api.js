e53f9b8fdafa6fb549d4f3468d3ed92396021714
Tirth A. Jadav <tirth.jadav04@gmail.com>
2026-02-09 12:37:14 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
- // baseURL: import.meta.env.VITE_API_URL
-  baseURL: "https://company-dashboard-backend-xa8w.onrender.com"
+  // This will pull from Vercel's env settings during build
+  baseURL: import.meta.env.VITE_API_URL || "https://company-dashboard-backend-xa8w.onrender.com"
 });
 
 export default API;
